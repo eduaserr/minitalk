0b4751549ad157f8aef3e232099f1fba2ed3051a
eduaserr <157403329+eduaserr@users.noreply.github.com>
2024-10-01 17:44:52 +0200

    signal_handler function finished, minitalk functional

--- src/server.c
+++ src/server.c
@@ -6,19 +6,40 @@
 /*   By: eduaserr < eduaserr@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/25 18:56:06 by eduaserr          #+#    #+#             */
-/*   Updated: 2024/09/30 16:30:48 by eduaserr         ###   ########.fr       */
+/*   Updated: 2024/10/01 17:30:42 by eduaserr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-int main()
+void	signal_handler(int signal)
+{
+	static char	chr;
+	static int	bit;
+
+	if (signal == SIGUSR2)
+		chr |= 1;
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", chr);
+		chr = 0;
+		bit = 0;
+	}
+	else
+		chr <<= 1;
+}
+
+int	main()
 {
 	pid_t	pid;
 
 	pid = getpid();
 	ft_printf("PID : %d\n", pid);
-	kill(pid, SIGUSR1);
-	kill(pid, SIGUSR2);
-	sigaction();
+	ft_printf("\033[5m\033[90mtok tok...\033[0m\n");
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
+	while (1)
+		pause();
+	return (0);
 }
